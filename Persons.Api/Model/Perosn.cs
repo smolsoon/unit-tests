73bada6f10bfba552202b0c587f781566559884e
Kamil Smolik <ksmolik@outlook.com>
2019-03-10 10:34:33 +0100

    Create a person model

---
+++ Persons.Api/Model/Perosn.cs
@@ -0,0 +1,18 @@
+namespace Persons.Api.Model
+{
+    public class Perosn
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public Person(Guid id, string firstname, string lastname, int age)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;                
+        }
+    }
+}
